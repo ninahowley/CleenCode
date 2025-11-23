3e050a036073c3a3ca05a3c9e1bec248666f5efd
Dianna <dg109@wellesley.edu>
2025-11-22 19:09:05 -0500

    for nina

--- methods.py
+++ methods.py
@@ -112,7 +112,8 @@ def isPalindrome(s: str) -> bool:
 """
 Given a string s, return true if it is a palindrome, otherwise return false.
 
-A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+A palindrome is a string that reads the same forward and backward. 
+It is also case-insensitive and ignores all non-alphanumeric characters.
 
 Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
 
